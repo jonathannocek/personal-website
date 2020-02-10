64892516931b1a3be7a6cb85ae6a36d6776dbecb
Jonathan Nocek <jnocek@iwu.edu>
2020-02-10 08:18:43 -0600

    Added intro

--- gatsby-starter-dimension/src/components/Main.js
+++ gatsby-starter-dimension/src/components/Main.js
@@ -29,28 +29,18 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Intro</h2>
-          <span className="image main">
-            <img src={pic01} alt="" />
-          </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#experience">awesome work</a>.
+            Hi! My name is Jonathan Nocek and I am currently a senior at Illinois Wesleyan 
+            University pursuing a major in Computer Science with a minor in Data Science.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+            This past summer, I worked at Discover Financial Services as a Data Engineering Intern 
+            in the Advanced Analytics department. After graduation, I will be returning to Discover 
+            to continue my work as a Data Engineer.
+          </p>
+          <p>
+            Outside of school and work, I am a member of the Men's Golf Team at IWU where we 
+             won the 2019 Division III NCAA Men's Golf Championship!
           </p>
           {close}
         </article>
